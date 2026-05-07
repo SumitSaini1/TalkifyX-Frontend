1941e2b3e1e985e0b9b467d645327408e60a903f
Sumit Saini <146309022+SumitSaini1@users.noreply.github.com>
2026-05-07 23:29:48 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,12 +3,12 @@ export const environment = {
   apiBaseUrl: 'http://13.206.79.87:8080',
   wsUrl: 'http://13.206.79.87:8085/ws',
   firebase: {
-    apiKey: 'REPLACE_WITH_YOUR_FIREBASE_API_KEY',
-    authDomain: 'REPLACE_WITH_YOUR_AUTH_DOMAIN',
-    projectId: 'REPLACE_WITH_YOUR_PROJECT_ID',
-    storageBucket: 'REPLACE_WITH_YOUR_STORAGE_BUCKET',
-    messagingSenderId: 'REPLACE_WITH_YOUR_SENDER_ID',
-    appId: 'REPLACE_WITH_YOUR_APP_ID',
-    vapidKey: 'REPLACE_WITH_YOUR_VAPID_KEY',
+    apiKey: "AIzaSyC7wzEitced9snKBRPkoyTNkVoxNIVgoc",
+    authDomain: "talkifyx-30bb6.firebaseapp.com",
+    projectId: "talkifyx-30bb6",
+    storageBucket: "talkifyx-30bb6.firebasestorage.app",
+    messagingSenderId: "409946370724",
+    appId: "1:409946370724:web:99b9d09884b3e8a238ac31",
+    vapidKey:"BJA4MhzfJeOBqqP6ltZb25q-Pwkt8YvkDhCK0P1DZSAxlzlJod9KUOvvBZMFX4uJ2ake3vpegq2Pl-6T-3HBo-k"
   },
 };
