54c3e57063d601271a01ac3dac31d002e70df036
SumitSaini1 <sumitsaini21027@gmail.com>
2026-05-14 13:01:58 +0530

    [Sumit Saini] add: environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://13.206.79.87:8080',
-  wsUrl: 'http://13.206.79.87:8085/ws',
+  apiBaseUrl: "https://talkifyx.duckdns.org",
+  wsUrl: "wss://talkifyx.duckdns.org/ws",
   firebase: {
     apiKey: "AIzaSyC7wzEitced9snKBRPkoyTNkVoxNIVgoc",
     authDomain: "talkifyx-30bb6.firebaseapp.com",
@@ -9,6 +9,7 @@ export const environment = {
     storageBucket: "talkifyx-30bb6.firebasestorage.app",
     messagingSenderId: "409946370724",
     appId: "1:409946370724:web:99b9d09884b3e8a238ac31",
-    vapidKey:"BJA4MhzfJeOBqqP6ltZb25q-Pwkt8YvkDhCK0P1DZSAxlzlJod9KUOvvBZMFX4uJ2ake3vpegq2Pl-6T-3HBo-k"
+    vapidKey:
+      "BJA4MhzfJeOBqqP6ltZb25q-Pwkt8YvkDhCK0P1DZSAxlzlJod9KUOvvBZMFX4uJ2ake3vpegq2Pl-6T-3HBo-k",
   },
 };
