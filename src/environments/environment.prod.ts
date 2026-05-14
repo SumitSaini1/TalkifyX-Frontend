4194cfce616374be2103fb952fbe471d3220db45
SumitSaini1 <sumitsaini21027@gmail.com>
2026-05-14 13:55:42 +0530

    [Sumit Saini] add : oauth url in prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: true,
   apiBaseUrl: "https://talkifyx.duckdns.org",
   wsUrl: "wss://talkifyx.duckdns.org/ws",
+  oauthGoogleUrl: "https://talkifyx.duckdns.org/oauth2/authorization/google",
   firebase: {
     apiKey: "AIzaSyC7wzEitced9snKBRPkoyTNkVoxNIVgoc",
     authDomain: "talkifyx-30bb6.firebaseapp.com",
